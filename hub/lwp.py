3b1a5ab5af290b257e067d56431d77a9b1bbfef5
Michał Szafrański <szafranski@gmail.com>
2020-01-30 23:23:02 +0100

    small cleanup

--- hub/lwp.py
+++ hub/lwp.py
@@ -23,9 +23,9 @@ class LWPDevice:
   def portMode(self, port, mode, notify = 1):
     self.send(bytes([0x41, port, mode, notify, 0, 0, 0, 1]))
 
-def connect(t = 15):
-  hub.ble.scan(t)
-  for sec in range(t):
+def connect(timeout = 15):
+  hub.ble.scan(timeout)
+  for _ in range(timeout):
     for i, a in enumerate(hub.ble.scan_result()):
       if a['service_id'] == '00001623-1212-EFDE-1623-785FEABCD123':
         hub_model_id = a['man_data'][1] if len(a['man_data']) > 1 else None
@@ -36,8 +36,3 @@ def connect(t = 15):
         return None
     time.sleep(1)
   return None
-
-
-# conn.subscribe()
-# setled  = b'\x08\x00\x81\x32\x11\x51\x00\x04' lwp.writePort1(z, 0x32, 0, 4)
-# setport = b'\x0A\x00\x41\x32\x00\x01\x00\x00\x00\x01'
