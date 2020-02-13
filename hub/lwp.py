62737bbfedffe1d92b4cc7ac4ee8e195fb6e8c4d
Michał Szafrański <szafranski@gmail.com>
2020-02-13 22:46:05 +0100

    button handling

--- hub/lwp.py
+++ hub/lwp.py
@@ -2,11 +2,12 @@ import hub
 import time
 import binascii
 
-DUPLO_TRAIN_HUB_ID = 32
-BOOST_MOVE_HUB_ID = 64
-POWERED_UP_HUB_ID = 65
-POWERED_UP_REMOTE_ID = 66
-CONTROL_PLUS_LARGE_HUB_ID = 128
+class HubType:
+  DUPLO_TRAIN_HUB_ID = 32
+  BOOST_MOVE_HUB_ID = 64
+  POWERED_UP_HUB_ID = 65
+  POWERED_UP_REMOTE_ID = 66
+  CONTROL_PLUS_LARGE_HUB_ID = 128
 
 _connected_idx = None
 def _connect_callback(idx):
@@ -18,6 +19,36 @@ hub.ble.callback(_connect_callback)
 def _hexlify(bytes):
   return binascii.hexlify(bytes, ' ').decode()
 
+class LWPButton:
+  def __init__(self):
+    self._callback = None
+    self._pressed = False
+    self._was_pressed = False
+    self._last_pressed_ms = 0
+  def on_change(self, cb):
+    self._callback = cb
+  def is_pressed(self):
+    return self._pressed
+  def was_pressed(self):
+    r = self._was_pressed
+    self._was_pressed = False
+    return r
+  def _change(self, value):
+    if self._pressed != value:
+      self._pressed = value
+      if value:
+        self._was_pressed = True
+        self._last_pressed_ms = time.ticks_ms()
+        if self._callback:
+          self._callback(0)
+      else:
+        if self._callback:
+          l = time.ticks_diff(time.ticks_ms(), self._last_pressed_ms)
+          self._callback(l if l > 0 else 1)
+
+class LWPObject:
+  pass
+
 class LWPDevice:
   def __init__(self, conn, model_id, idx):
     self.conn = conn
@@ -25,13 +56,26 @@ class LWPDevice:
     self.model_id = model_id
     self.conn.callback(self.recv)
     self.conn.subscribe()
+    self.button = LWPObject()
     time.sleep(.1)
-    if model_id == DUPLO_TRAIN_HUB_ID:
+    if model_id == HubType.DUPLO_TRAIN_HUB_ID:
       self.ledPort = 0x11
-    elif model_id == POWERED_UP_REMOTE_ID:
+    elif model_id == HubType.POWERED_UP_REMOTE_ID:
+      self.button.A = LWPObject()
+      self.button.B = LWPObject()
+      self.button.A.plus = LWPButton()
+      self.button.A.red = LWPButton()
+      self.button.A.minus = LWPButton()
+      self.button.B.plus = LWPButton()
+      self.button.B.red = LWPButton()
+      self.button.B.minus = LWPButton()
       self.ledPort = 0x34
+      self.portMode(0, 3)
+      self.portMode(1, 3)
     else:
       self.ledPort = 0x32
+    self.button.green = LWPButton()
+    self.subscribeToHubProp(2)
 
   def disconnect(self):
     self.conn.disconnect(self.idx)
@@ -43,10 +87,20 @@ class LWPDevice:
     elif msg_type == 0x05: # error
       print("error %d: cmd %02x code %02x" % (self.idx, data[3], data[4]))
     elif msg_type == 0x45: # port value (single)
-      print("port value %d: port %02x" % (self.idx, data[3]), _hexlify(data[4:]))
+      port = data[3]
+      print("port value %d: port %02x" % (self.idx, port), _hexlify(data[4:]))
+      if self.model_id == HubType.POWERED_UP_REMOTE_ID and port < 2:
+        b = self.button.A if port == 0 else self.button.B
+        b.plus._change((data[4] >> 0) & 1)
+        b.red._change((data[4] >> 1) & 1)
+        b.minus._change((data[4] >> 2) & 1)
     elif msg_type == 0x47: # port input format (single)
       interval = int.from_bytes(data[5:8], 'little', False)
       print("port mode %d: port %02x mode %02x notify %02x interval %d" % (self.idx, data[3], data[4], data[9], interval))
+    elif msg_type == 0x01: # hub property update
+      print("hub action %d: prop %02x value" % (self.idx, data[3]), _hexlify(data[5:]))
+      if data[3] == 2:
+        self.button.green._change(data[5])
     elif msg_type == 0x02: # hub action
       print("hub action %d: %02x" % (self.idx, data[3]))
     elif msg_type == 0x04: # port event
@@ -65,16 +119,25 @@ class LWPDevice:
 
   def send(self, data):
     self.conn.send((len(data)+2).to_bytes(2, 'little') + data)
-    time.sleep(.01)
+    time.sleep(.2)
 
   def writePort(self, port, mode, data):
     self.send(bytes([0x81, port, 0x11, 0x51, mode]) + data)
 
   def writePort1(self, port, mode, b):
-    self.writePort(port, mode, bytes([b]))
+    self.writePort(port, mode, bytes([b & 255]))
 
   def portMode(self, port, mode, notify = 1):
     self.send(bytes([0x41, port, mode, 1, 0, 0, 0, notify]))
+
+  def setHubProp(self, prop, data):
+    self.send(bytes([0x01, prop, 0x01]) + data)
+
+  def getHubProp(self, prop):
+    self.send(bytes([0x01, prop, 0x05]))
+
+  def subscribeToHubProp(self, prop):
+    self.send(bytes([0x01, prop, 0x02]))
 
   def off(self):
     self.send(bytes([0x02, 0x01]))
